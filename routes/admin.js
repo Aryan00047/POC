42a5ee791b498544afd550a2773df27bf6980298
Aryan Gupta <aryan.gupta@happiestminds.com>
2024-10-29 13:00:18 +0530

    Admin route is working properly

--- routes/admin.js
+++ routes/admin.js
@@ -8,4 +8,10 @@ console.log("admin routes accessed")
 // Admin login
 router.post('/login', adminController.loginAdmin);
 
+//fetch candidates
+router.post('/candidates', authMiddleware.verifyTokenAdmin, adminController.fetchCandidates)
+
+//fetch hr
+router.post('/hr', authMiddleware.verifyTokenAdmin, adminController.fetchHr)
+
 module.exports = router;
